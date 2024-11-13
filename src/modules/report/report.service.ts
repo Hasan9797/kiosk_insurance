399981285cfb587f62f639780f0449e861b8e182
rthcoder <tohirdavrsmart@gmail.com>
2024-11-13 11:57:23 +0500

    fx

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -33,7 +33,7 @@ export class ReportService {
                   }
                 },
                 include: {
-                  userBalances: true
+                  userBalance: true
                 }
               }
             }
